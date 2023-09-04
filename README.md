a05e6f83aaddd968defdc56bbf60a46661d24a78
Luiz Carlos <dia.a.diajj@gmail.com>
2023-09-04 14:01:41 -0300

    update files

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Sistema-de-Alarme
 Sistema de Alarme com Visão Computacional em Python, usando o cv2 e Yolov8.
+
+🚀 Começando
+Essas instruções permitirão que você obtenha uma cópia do projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+Consulte Implantação para saber como implantar o projeto.
+
+📋 Pré-requisitos
+* Importações Necessárias:
+    - import cv2 
+    - from ultralytics import YOLO
+    - import winsound
+    - import threading
+
+🔧 Instalação
+Uma série de exemplos passo-a-passo que informam o que você deve executar para ter um ambiente de desenvolvimento em execução.
+
+ - pip install opencv2-python
+ - pip install ultralytics
+
+
+📦 Implantação
+
+    * Linha 8 - Adicionar os valores para a imagem e local que você irá capturar (eixo X e Y)
+    * linha 36 - Aqui você pode adiconar o valor para o cls == ? que você desejar , baseado no file coco_names.txt
+
+
+🛠️ Construído com
+
+* Visual Studio Code
+* Webcam DroidCam
+
+📌 Versão
+1.0 
+
+
+✒️ Autores
+Eu mesmo! @luizcarlosjj.
+
+
+📄 Licença
+Este projeto está sob a licença MIT LICENSE - veja o arquivo LICENSE.md para detalhes.
+
+
+⌨️ com ❤️
---
+++ pessoa_rec.py
@@ -0,0 +1,58 @@
+import cv2 
+from ultralytics import YOLO
+import winsound
+import threading
+
+video = cv2.VideoCapture(0)
+modelo = YOLO('yolov8n.pt')
+area = [230, 170, 360, 440]
+
+alarme_ctl = False
+
+def alarme():
+    global alarme_ctl
+    for _ in range(7):
+        winsound.Beep(2500,500)
+        
+
+    alarme_ctl = False
+
+
+
+while True:
+    check, img = video.read()
+    img2 = img.copy()
+    cv2.rectangle(img2, (area[0], area[1]), (area[2], area[3]), (0,255,0), -1)
+    resultado = modelo(img)
+
+    for objetos in resultado:
+        obj = objetos.boxes
+        for dados in obj:
+            x,y,w,h = dados.xyxy[0]
+            x,y,w,h = int(x), int(y), int(w), int(h)
+            cls = int(dados.cls[0])
+            # centro da área de interesse:
+            cx, cy = (x+w)//2, (y+h)//2 
+            if cls == 0:
+                cv2.rectangle(img, (x, y), (w, h), (255,0,0), 5)
+
+                if cx>=area[0] and cx<=area[2] and cy>=area[1] and cy<=area[3]:
+                    cv2.rectangle(img2, (area[0], area[1]), (area[2], area[3]), (0,0,255), -1)
+                    cv2.rectangle(img, (70,20), (400, 60), (0,0,255), -1)
+                    cv2.putText(img, "Invasor Detectado", (100,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)       
+                    if not alarme_ctl:
+                        alarme_ctl = True
+                        threading.Thread(target=alarme).start()
+
+
+             
+ 
+    imgFinal = cv2.addWeighted(img2, 0.5, img, 0.5, 0)
+
+    cv2.imshow('img', imgFinal)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+video.release()
+cv2.destroyAllWindows()
